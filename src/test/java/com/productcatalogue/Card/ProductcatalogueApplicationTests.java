b4513990ccda62e2d4aca755da901cd7ee6fde67
pthotaku <pthotaku@cisco.com>
2020-07-06 11:40:45 +0530

    intial project setting up

---
+++ src/test/java/com/productcatalogue/Card/ProductcatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productcatalogue.Card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
