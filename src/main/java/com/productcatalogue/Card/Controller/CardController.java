ba037af233e36e05663c86faba5c3c7e2a29a369
pthotaku <pthotaku@cisco.com>
2020-07-18 01:16:46 +0530

    url change issue

--- src/main/java/com/productcatalogue/Card/Controller/CardController.java
+++ src/main/java/com/productcatalogue/Card/Controller/CardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.productcatalogue.Card.Dto.CardDto;
 import com.productcatalogue.Card.Service.CardService;
 
-@RestController(value = "/v1/api")
+@RestController
 public class CardController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CardController.class);
@@ -21,21 +21,21 @@ public class CardController {
 	CardService cardService;
 
 
-	@RequestMapping(value = "/addtocard", method = RequestMethod.POST,
+	@RequestMapping(value = "/api/v1/addtocard", method = RequestMethod.POST,
 			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void addToCard(@RequestBody CardDto cardDto) {
 		LOG.info("Request Triggered in addToCard");
 		cardService.addInCard(cardDto);
 	}
 
-	@RequestMapping(value = "/addtocard", method = RequestMethod.PUT,
+	@RequestMapping(value = "/api/v1/addtocard", method = RequestMethod.PUT,
 			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void updateToCard(@RequestBody CardDto cardDto) {
 		LOG.info("Request Triggered in updateToCard");
 		cardService.UpdatetoCard(cardDto);
 	}
 
-	@RequestMapping(value = "/addtocard", method = RequestMethod.DELETE,
+	@RequestMapping(value = "/api/v1/addtocard", method = RequestMethod.DELETE,
 			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void DeleteInCard(@RequestBody CardDto cardDto) {
 		LOG.info("Request Triggered in DeleteInCard");
